06a76876a704847873cb19177520c79f67774b73
samanthayee18 <samanthayee@csu.fullerton.edu>
2020-05-23 02:24:12 -0700

    Delete header.php

--- header.php
+++
@@ -1,5 +0,0 @@
-
-<div class="jumbotron">
-    <h1 class="display-4">Welcome to Quota!</h1>
-    <p class="lead">Add the Quote cards to you library where you can save and share from your profile!</p>
-</div>
